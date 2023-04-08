a31b7eb209f0e4a5b3a39cbcf31ff7a8cc7e26b1
Rohan Batra <github@rohanbatra.in>
2023-04-08 13:40:19 +0530

    adding st

--- Proxmox/Start-here.sh
+++ Proxmox/Start-here.sh
@@ -10,13 +10,15 @@ cd /root/Builds
 # Getting archives
 wget https://dl.suckless.org/dwm/dwm-6.4.tar.gz 
 wget https://dl.suckless.org/tools/dmenu-5.2.tar.gz
+wget https://dl.suckless.org/st/st-0.9.tar.gz
 
 # extracting them
 tar -xf dwm-*.tar.gz 
 tar -xf dmenu-*.tar.gz
+tar -xf st-*.tar.gz
 
 # Building them at once
-for i in d* ; do cd $i ; make clean install ; cd .. ; done
+for i in * ; do cd $i ; make clean install ; cd .. ; done
 
 # Going back to home of root
 cd /root
