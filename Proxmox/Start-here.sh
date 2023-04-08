1a15c37bce36457f3fa2baa25179c9245d119681
Rohan Batra <github@rohanbatra.in>
2023-04-09 00:26:28 +0530

    Update Start-here.sh

--- Proxmox/Start-here.sh
+++ Proxmox/Start-here.sh
@@ -1,47 +1,19 @@
 #!/bin/bash
 
+# Updating the repos
+apt update
+
 # Fetching aptitude packages
 apt install sudo make gcc libx11-dev libxft-dev libxinerama-dev xorg firefox-esr pulseaudio pavucontrol
 
 # trying audio
 systemctl --user enable pulseaudio.service
-pulseaudio --start
-
 
 # Making a new user
-adduser rohan 
+adduser rohan
 usermod -aG sudo rohan
-su rohan
-
-
-
-# Making a build directory
-mkdir /home/rohan/Builds
-cd /home/rohan/Builds
-
-# Getting archives
-wget https://dl.suckless.org/dwm/dwm-6.4.tar.gz 
-wget https://dl.suckless.org/tools/dmenu-5.2.tar.gz
-wget https://dl.suckless.org/st/st-0.9.tar.gz
-
-# extracting them
-tar -xf dwm-*.tar.gz 
-tar -xf dmenu-*.tar.gz
-tar -xf st-*.tar.gz
-
-# Building them at once
-for i in * ; do cd $i ; sudo make clean install ; cd .. ; done
-
-# Going back to home of rohan
 cd /home/rohan
-
-# Fetching proxmox dot files
-rm .xinitrc
-wget https://raw.githubusercontent.com/rohanbatrain/dot-files/main/proxmox/.xinitrc
-
-# Configuring appimages
-mkdir Applications
-cd Applications
-wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.1.16/Obsidian-1.1.16.AppImage
-wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage
-chmod +x *.AppImage
+#git clone here
+chmod +x Part-2.sh
+cp Part-2.sh /home/rohan
+su rohan
