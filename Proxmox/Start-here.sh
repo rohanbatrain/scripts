2b43bf18ff247bde9a03c44f6129287b1a7ae0d2
Rohan Batra <github@rohanbatra.in>
2023-04-08 14:10:25 +0530

    added rohan as a non root user

--- Proxmox/Start-here.sh
+++ Proxmox/Start-here.sh
@@ -3,9 +3,14 @@
 # Fetching aptitude packages
 apt install make gcc libx11-dev libxft-dev libxinerama-dev xorg
 
+# Making a new user
+adduser rohan 
+usermod -aG sudo rohan
+su rohan
+
 # Making a build directory
-mkdir /root/Builds
-cd /root/Builds
+mkdir /home/rohan/Builds
+cd /home/rohan/Builds
 
 # Getting archives
 wget https://dl.suckless.org/dwm/dwm-6.4.tar.gz 
@@ -18,10 +23,10 @@ tar -xf dmenu-*.tar.gz
 tar -xf st-*.tar.gz
 
 # Building them at once
-for i in * ; do cd $i ; make clean install ; cd .. ; done
+for i in * ; do cd $i ; sudo make clean install ; cd .. ; done
 
-# Going back to home of root
-cd /root
+# Going back to home of rohan
+cd /home/rohan
 
 # Fetching proxmox dot files
 rm .xinitrc
@@ -31,3 +36,5 @@ wget https://raw.githubusercontent.com/rohanbatrain/dot-files/main/proxmox/.xini
 mkdir Applications
 cd Applications
 wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.1.16/Obsidian-1.1.16.AppImage
+wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage
+chmod +x *.AppImage
