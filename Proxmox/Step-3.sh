c5e1c6e4a3f875da4d4db19def8f4d532bd0c5a0
Rohan Batra <github@rohanbatra.in>
2023-04-09 15:15:01 +0530

    Update Step-3.sh

--- Proxmox/Step-3.sh
+++ Proxmox/Step-3.sh
@@ -6,5 +6,4 @@
 cd /tmp
 wget  https://sgpfiles.netgate.com/mirror/downloads/pfSense-CE-2.6.0-RELEASE-amd64.iso.gz
 gzip -d pfSense-CE-2.6.0-RELEASE-amd64.iso.gz
-cp pfSense-CE-*.iso /var/lib/vz/template/iso/
-
+rsync -aP --remove-source-files pfSense-CE-2.6.0-RELEASE-amd64.iso /var/lib/vz/template/iso/
