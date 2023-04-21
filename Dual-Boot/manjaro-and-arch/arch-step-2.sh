962973749970eabaa1a73e83b2a4f7307bc753c3
Rohan Batra <github@rohanbatra.in>
2023-04-21 22:53:01 +0530

    Update arch-step-2.sh

--- Dual-Boot/manjaro-and-arch/arch-step-2.sh
+++ Dual-Boot/manjaro-and-arch/arch-step-2.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 
+timedatectl set-timezone Asia/Kolkata
+sed -i 's/#en_IN/en_IN/' /etc/locale.gen
+locale-gen
+echo LANG=en_IN UTF-8 > /etc/locale.conf
+export LANG=en_IN UTF-8
+echo rohanbatra > /etc/hostname
+touch /etc/hosts
+echo "127.0.0.1	localhost" >> /etc/hosts
+echo "::1		localhost" >> /etc/hosts
+echo "127.0.1.1	rohanbatra" >> /etc/hosts
+echo "Please set your root password"
+passwd
