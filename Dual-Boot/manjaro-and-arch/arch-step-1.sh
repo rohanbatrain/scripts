e5f845d900f9628c814e0a9bc238dc7987b62948
Rohan Batra <github@rohanbatra.in>
2023-04-21 20:15:57 +0530

    Update arch-step-1.sh

--- Dual-Boot/manjaro-and-arch/arch-step-1.sh
+++ Dual-Boot/manjaro-and-arch/arch-step-1.sh
@@ -3,16 +3,16 @@ lsblk
 
 read -p "Please enter the drive name: " drive_name
 echo "You have to make 5 partitions, which are as follows"
-echo "Partition-1 /dev/sda1 512M BOOT"
-echo "Partition-2 /dev/sda2 31G SWAP"
-echo "Partition-3 /dev/sda3 100G ARCH ROOT"
-echo "Partition-4 /dev/sda4 100G MANJARO (ALL FILES IN ONE PART)"
-echo "Partition-5 /dev/sda5 700G HOME DIRECTORY"
+echo "Partition-1 512M BOOT"
+echo "Partition-2 31G SWAP"
+echo "Partition-3 100G ARCH ROOT"
+echo "Partition-4 100G MANJARO (ALL FILES IN ONE PART)"
+echo "Partition-5 700G HOME DIRECTORY"
 read -p "Are the instructions clear? Y/N: " choice_1
 
-if [$choice == "Y"]; then
+if [[ $choice_1 == "Y" ]]; then
   continue
-elif [$choice == "N"]; then
+elif [[ $choice_1 == "N" ]]; then
   echo "Exiting, user choice is not positive."
   exit
 else
@@ -20,4 +20,4 @@ else
   exit
 fi
 
-cfdisk $drive_name
+cfdisk ${drive_name}
