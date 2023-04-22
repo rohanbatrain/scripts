4394e1c5a398e987b043c911739120241b02fab0
Rohan Batra <github@rohanbatra.in>
2023-04-22 16:00:34 +0530

    Update arch-step-2.sh

--- Dual-Boot/manjaro-and-arch/arch-step-2.sh
+++ Dual-Boot/manjaro-and-arch/arch-step-2.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Pacman optimisations
+sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
+yes | pacman -Syy reflector rsync curl wget xorg networkmanager
+reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
+# Continue
 timedatectl set-timezone Asia/Kolkata
 sed -i 's/#en_IN/en_IN/' /etc/locale.gen
 locale-gen
@@ -13,3 +18,7 @@ echo "::1		localhost" >> /etc/hosts
 echo "127.0.1.1	rohanbatra" >> /etc/hosts
 echo "Please set your root password"
 passwd
+useradd -m rohan
+passwd rohan
+EDITOR=vim
+systemctl enable NetworkManager.service
