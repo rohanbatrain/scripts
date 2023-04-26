bff16f5c6bbd40bf8d87b9bb7c04861127874898
Rohan Batra <github@rohanbatra.in>
2023-04-26 17:37:01 +0530

    Update arch-step-2.sh
    incomplete

--- Dual-Boot/manjaro-and-arch/arch-step-2.sh
+++ Dual-Boot/manjaro-and-arch/arch-step-2.sh
@@ -30,7 +30,9 @@ systemctl enable NetworkManager.service
 mkdir /mnt/rohan
 
 # appending home fir to arch
-for i in $(blkid|grep sda4); do echo $1 /mnt/rohan ext4 defaults 0 1 ; done | grep -w UUID | sed "s/\"//g" >> /etc/fstab 
+for i in $(blkid|grep sda4); do echo $1 /mnt/rohan ext4 defaults 0 1 ; done | grep -w UUID | sed "s/\"//g" > new
+
+
 
 
 
