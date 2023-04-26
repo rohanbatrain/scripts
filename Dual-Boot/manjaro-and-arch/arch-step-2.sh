e121b224ff25a57bc20fa980acbd0e73be7f2db4
Rohan Batra <github@rohanbatra.in>
2023-04-26 16:58:59 +0530

    Update arch-step-2.sh

--- Dual-Boot/manjaro-and-arch/arch-step-2.sh
+++ Dual-Boot/manjaro-and-arch/arch-step-2.sh
@@ -9,8 +9,8 @@ reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorli
 timedatectl set-timezone Asia/Kolkata
 sed -i 's/#en_IN/en_IN/' /etc/locale.gen
 locale-gen
-echo LANG=en_IN UTF-8 > /etc/locale.conf
-export LANG=en_IN UTF-8
+echo LANG=en_IN.UTF-8 > /etc/locale.conf
+export LANG=en_IN.UTF-8
 echo rohanbatra > /etc/hostname
 touch /etc/hosts
 echo "127.0.0.1	localhost" >> /etc/hosts
