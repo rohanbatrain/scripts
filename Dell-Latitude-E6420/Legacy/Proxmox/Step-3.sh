ab85cbe99c47973fc19a88bdb90b9708b75186bc
Rohan Batra <github@rohanbatra.in>
2023-04-30 17:50:10 +0530

    Update Step-3.sh

--- Dell-Latitude-E6420/Legacy/Proxmox/Step-3.sh
+++ Dell-Latitude-E6420/Legacy/Proxmox/Step-3.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+# Should be executed by root
+
+# pfsense iso in proxmox
+cd /tmp
+wget  https://sgpfiles.netgate.com/mirror/downloads/pfSense-CE-2.6.0-RELEASE-amd64.iso.gz
+gzip -d pfSense-CE-2.6.0-RELEASE-amd64.iso.gz
+rsync -aP --remove-source-files pfSense-CE-2.6.0-RELEASE-amd64.iso /var/lib/vz/template/iso/
