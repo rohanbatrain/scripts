65ba4be1bc17e1cd81b738ef25e5e8cfd1d3f12a
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-05-03 17:43:57 +0530

    Update Step-1.sh

--- One-plus-8t/Termux/Step-1.sh
+++ One-plus-8t/Termux/Step-1.sh
@@ -8,7 +8,7 @@ read GIT_EMAIL
 apt update && apt upgrade
 
 # saying yes to all conflicts (install the package maintainer's version)
-pkg install openssh tsu git
+pkg install openssh tsu git gnupg
 
 # Storage Access
 termux-setup-storage
@@ -20,3 +20,10 @@ mkdir -p /sdcard/Obsidian/Second-Brain/
 cd ~/.ssh/
 ssh-keygen -t rsa -C $GIT_EMAIL 
 
+# generating gpg keys
+gpg --full-generate-key # RSA-and-RSA, 4096, 0, ## Email: Github private email from setting should be used to generate gpg, 
+gpg --list-secret-keys --keyid-format=long
+## reading GPG-Key ID from stdout
+echo "Please copy and paste the Key-ID you want to use"
+read GPG_KEY_ID
+gpg --armor --export  $GPG_KEY_ID
