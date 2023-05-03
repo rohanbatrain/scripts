e72d3a17f12c2b3fb47e9718167fcaa8a9ba3de6
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-05-03 17:55:48 +0530

    Update Step-1.sh

--- One-plus-8t/Termux/Step-1.sh
+++ One-plus-8t/Termux/Step-1.sh
@@ -16,14 +16,18 @@ termux-setup-storage
 # Obsidian Specific
 mkdir -p /sdcard/Obsidian/Second-Brain/
 
-# Setting up git
-cd ~/.ssh/
-ssh-keygen -t rsa -C $GIT_EMAIL 
-
 # generating gpg keys
 gpg --full-generate-key # RSA-and-RSA, 4096, 0, ## Email: Github private email from setting should be used to generate gpg, 
 gpg --list-secret-keys --keyid-format=long
 ## reading GPG-Key ID from stdout
-echo "Please copy and paste the Key-ID you want to use"
-read GPG_KEY_ID
-gpg --armor --export  $GPG_KEY_ID
+echo "Please copy and paste the GPG primary key ID that you want to use"
+read GPG_PRIMARY_KEY_ID
+echo "Please copy and paste the GPG sub key id that you want to use"
+read GPG_SUB_KEY_ID
+gpg --armor --export  $GPG_PRIMARY_KEY_ID
+
+# Setting up git
+cd ~/.ssh/
+ssh-keygen -t rsa -C $GIT_EMAIL 
+git config --global user.signingkey $GPG_PRIMARY_KEY_ID
+git config --global user.signingkey "$GPG_SUB_KEY_ID\!"
