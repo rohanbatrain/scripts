79c8d84fb492f15423c257b3c3093710f3c807b3
Rohan Batra <github@rohanbatra.in>
2023-05-03 17:22:17 +0530

    Update Step-1.sh

--- One-plus-8t/Termux/Step-1.sh
+++ One-plus-8t/Termux/Step-1.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+# Setting up environment variables
+echo "Please enter your Github Email Address"
+read GIT_EMAIL
+
+# Updating the repositories
 apt update && apt upgrade
 
 # saying yes to all conflicts (install the package maintainer's version)
-
 pkg install openssh tsu git
 
-# Obsidian Specific
+# Storage Access
+termux-setup-storage
 
-sudo mkdir -p /sdcard/Obsidian/Second-Brain/
+# Obsidian Specific
+mkdir -p /sdcard/Obsidian/Second-Brain/
+
+# Setting up git
+cd ~/.ssh/
+ssh-keygen -t rsa -C $GIT_EMAIL 
 
