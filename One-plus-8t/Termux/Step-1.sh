e111b8db807677ad52f99a23defecdb685deeda1
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-05-07 17:07:59 +0530

    Update Step-1.sh

--- One-plus-8t/Termux/Step-1.sh
+++ One-plus-8t/Termux/Step-1.sh
@@ -47,7 +47,7 @@ ssh -T git@github.com
 # Fetching git repos 
 cd /data/data/com.termux/files/home
 mkdir -p Github/Repositories
-git clone --recurse-submodules git@github.com:rohanbatrain/Dot_Files.git /data/data/com.termux/files/home/Github/Repositories/Dot_Files/
+git clone --recurse-submodules git@github.com:rohanbatrain/Dot-Files.git /data/data/com.termux/files/home/Github/Repositories/Dot-Files/
 mkdir -p /sdcard/Obsidian/Second-Brain-Private/
 ln -s /sdcard/Obsidian/Second-Brain-Private/ /data/data/com.termux/files/home/Github/Repositories/.
 git clone --recurse-submodules git@github.com:rohanbatrain/Second-Brain-Private.git /data/data/com.termux/files/home/Github/Repositories/Second-Brain-Private/
