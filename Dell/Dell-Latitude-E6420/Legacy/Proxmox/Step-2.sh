fa66dd3d4534c1f852c8c1bddfbac055f8ffff27
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-05-17 08:24:17 +0530

    Update Step-2.sh

--- Dell/Dell-Latitude-E6420/Legacy/Proxmox/Step-2.sh
+++ Dell/Dell-Latitude-E6420/Legacy/Proxmox/Step-2.sh
@@ -18,11 +18,27 @@ git clone https://github.com/rohanbatrain/suckless-st.git
 
 git clone https://github.com/rohanbatrain/suckless-dwm.git
 
-# Building them at once
+# Building them 
 
-for i in * ; do cd $i ; sudo make install ; cd .. ; done
-
+## DWM
+cd suckless-dwm/V6.4/
+sudo make clean install
 cd ..
+
+## DMENU
+cd suckless-dmenu/V5.2/
+sudo make clean install
+cd ..
+
+## ST
+cd suckless-st/V0.9/
+sudo make clean install
+cd ..
+
+
+# Getting out of build directory
+cd ..
+
 
 # Fetching proxmox dot files
 
