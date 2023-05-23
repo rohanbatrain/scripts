0f3aa26e293a387d4550934d5b42f41fc891a4bc
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-05-23 14:17:15 +0530

    Update 00-SSH-Keys.sh

--- One-Plus/One-plus-8t/Production/Scripts/Termux/00-SSH-Keys.sh
+++ One-Plus/One-plus-8t/Production/Scripts/Termux/00-SSH-Keys.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
 # Setting up python server on host machine to clone ssh keys on folder
-read -p "Please paste the url of the directory containing zip of both the keys, please include / at the end" SSH_KEYS_URL
+
+read -p "Do you have your termux storage already setup? Y/N: " STORAGE_SETUP_CONFIRMATION
+
+if [STORAGE_SETUP_CONFIRMATION == Y ]
+then 
+     echo "Skipping termux storage command, system already configured! "
+else
+     termux-setup-storage
+fi
+
+read -p "Please paste the url of the directory containing zip of both the keys, please include / at the end : " SSH_KEYS_URL
 apt update && apt upgrade
 
