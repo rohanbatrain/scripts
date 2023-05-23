2171defd2edc1a6aa4d5912a466af849488c78dd
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-05-23 23:19:39 +0530

    Update 00-SSH-Keys.sh

--- One-Plus/One-plus-8t/Production/Scripts/Termux/00-SSH-Keys.sh
+++ One-Plus/One-plus-8t/Production/Scripts/Termux/00-SSH-Keys.sh
@@ -3,13 +3,11 @@
 
 read -p "Do you have your termux storage already setup? Y/N: " STORAGE_SETUP_CONFIRMATION
 
-if [STORAGE_SETUP_CONFIRMATION == Y ]
-then 
-     echo "Skipping termux storage command, system already configured! "
+if [ "$STORAGE_SETUP_CONFIRMATION" == "Y" ]; then
+    echo "Skipping termux storage command, system already configured!"
 else
-     termux-setup-storage
+    termux-setup-storage
 fi
 
-read -p "Please paste the url of the directory containing zip of both the keys, please include / at the end : " SSH_KEYS_URL
+read -p "Please paste the URL of the directory containing the zip of both the keys. Please include / at the end: " SSH_KEYS_URL
 apt update && apt upgrade
-
