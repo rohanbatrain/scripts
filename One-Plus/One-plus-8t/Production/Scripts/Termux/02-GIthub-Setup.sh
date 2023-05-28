1cc1bd9875481246550005b5988db912c89bd086
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-05-28 18:40:12 +0530

    Update 02-GIthub-Setup.sh

--- One-Plus/One-plus-8t/Production/Scripts/Termux/02-GIthub-Setup.sh
+++ One-Plus/One-plus-8t/Production/Scripts/Termux/02-GIthub-Setup.sh
@@ -5,4 +5,5 @@ git config --global user.name "Rohan Batra"
 git config --global user.email "116573125+rohanbatrain@users.noreply.github.com"
 git config --global gpg.format ssh
 git config --global commit.gpgsign true
+git config --global user.signingkey ~/.ssh/id_rsa.pub
 ssh -T git@github.com
