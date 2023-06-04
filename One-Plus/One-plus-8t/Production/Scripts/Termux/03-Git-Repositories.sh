7522fa333b9c2da21046a0a0795ed88c72c26203
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-06-04 11:58:11 +0530

    Update 03-Git-Repositories.sh

--- One-Plus/One-plus-8t/Production/Scripts/Termux/03-Git-Repositories.sh
+++ One-Plus/One-plus-8t/Production/Scripts/Termux/03-Git-Repositories.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 cd /data/data/com.termux/files/home
-mkdir -p Github/Repositories
+mkdir -p /storage/emulated/0/Github/Repositories/
+ln -s /storage/emulated/0/Github ./
+
+cd Github
 
 git clone --recurse-submodules git@github.com:rohanbatrain/Dot-Files.git /data/data/com.termux/files/home/Github/Repositories/Dot-Files/
 
-mkdir -p /sdcard/Obsidian/Second-Brain-Private/
+cd ..
+mkdir -p /storage/emulated/0/Obsidian/Second-Brain-Private/
 
-ln -s /sdcard/Obsidian/Second-Brain-Private/ /data/data/com.termux/files/home/Github/Repositories/.
+mkdir Obsidian
+ln -s /storage/emulated/0/Obsidian/Second-Brain-Private/ /data/data/com.termux/files/home/Obsidian/
 
 git clone --recurse-submodules git@github.com:rohanbatrain/Second-Brain-Private.git /data/data/com.termux/files/home/Github/Repositories/Second-Brain-Private/
