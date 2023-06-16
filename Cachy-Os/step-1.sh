e0eb32b4ab594ac7e4217ade58a04ad4a2eb624b
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-06-16 15:20:27 +0530

    Create step-1.sh

---
+++ Cachy-Os/step-1.sh
@@ -0,0 +1,15 @@
+# Prerequisities : copy your ssh keys on ~/.ssh/
+
+cd ~
+mkdir "Github" "Builds"
+sudo pacman -Syyu firefox python-pip yay veracrypt
+yay vscodium-bin
+cd ~/.ssh/
+chmod 700 ~/.ssh/
+chmod 600 ~/.ssh/id_rsa
+git config --global user.name "Rohan Batra"
+git config --global user.email "116573125+rohanbatrain@users.noreply.github.com"
+git config --global gpg.format ssh
+git config --global commit.gpgsign true
+git config --global user.signingkey ~/.ssh/id_rsa.pub
+ssh -T git@github.com
