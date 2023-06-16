94896bc307c4cb510427c104ef87725ca957885c
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-06-16 14:16:37 +0530

    Update 03-Git-Repositories.sh

--- Termux/03-Git-Repositories.sh
+++ Termux/03-Git-Repositories.sh
@@ -4,23 +4,11 @@
 cd /data/data/com.termux/files/home
 mkdir -p /storage/emulated/0/Github/Repositories/
 ln -s /storage/emulated/0/Github ./
+cd Github/Repositories
 
-cd Github
-
-git clone --recurse-submodules git@github.com:rohanbatrain/Dot-Files.git /data/data/com.termux/files/home/Github/Repositories/Dot-Files/
-
-cd ..
-mkdir -p /storage/emulated/0/Obsidian/Second-Brain-Private/
-
-mkdir Obsidian
-
-
-git clone --recurse-submodules git@github.com:rohanbatrain/Second-Brain-Private.git /storage/emulated/0/Obsidian/Second-Brain-Private/
-
-ln -s /storage/emulated/0/Obsidian/Second-Brain-Private/ /data/data/com.termux/files/home/Obsidian/
-
-
-
-cd Github/Repositories/
+# cloning repositories
+git clone --recurse-submodules git@github.com:rohanbatrain/Dot-Files.git 
+git clone --recurse-submodules git@github.com:rohanbatrain/Second-Brain-Private.git 
 git clone git@github.com:rohanbatrain/Second-Brain-Tools-2022.git
+
 cd ~
