8211a9417134ac9a7b0c15d1cbba2c7c50b8962b
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-08-27 16:27:34 +0530

    Update Step-3.sh

--- Proxmox/Post-Install/Configuration/Step-3.sh
+++ Proxmox/Post-Install/Configuration/Step-3.sh
@@ -2,6 +2,14 @@
 
 # Cloning all the git repositories under ~/Projects/ Directory.
 
+read -p "Have you copied the id_rsa and id_rsa.pub keys to your '~/.ssh' Directory? Y/N " SSH_CONFIRMATION
+
+if [ "$SSH_CONFIRMATION" == "Y" ]; then
+    echo "Thank you for the confirmation, ssh keys already configured!"
+else
+    break
+fi
+
 cd ~/Projects 
 git clone git@github.com:rohanbatrain/second-brain-api.git
 git clone git@github.com:rohanbatrain/Educational-Projects.git
