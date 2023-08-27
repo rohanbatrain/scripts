ae876d095d4affc78c1b24ff9c673bb3c5794651
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-08-27 19:31:45 +0530

    Create Step-4.sh

---
+++ Proxmox/Post-Install/Configuration/Step-4.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# We are going to symlink manjaro's Home directories, to be clear not the whole home directory rather it's required sub directories, why not the other way around, well proxmox is primarily gonna have a working full fledged VM so this dwm setup will not gonna be my main anyway, so instead of storing data here, i would store data on manjaro, which seems a better trade off.
+
