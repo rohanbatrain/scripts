eca00d9175ad22cf4c9866c19272f85b244296d4
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-08-27 16:04:02 +0530

    Create Step-3.sh

---
+++ Proxmox/Post-Install/Configuration/Step-3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Cloning all the git repositories under ~/Projects/ Directory.
+
+cd ~/Projects 
+git clone git@github.com:rohanbatrain/second-brain-api.git
+git clone git@github.com:rohanbatrain/Educational-Projects.git
+git clone git@github.com:rohanbatrain/productivity-suite.git
+git clone git@github.com:rohanbatrain/second-brain-template.git
+git clone git@github.com:rohanbatrain/dotfiles.git
+git clone git@github.com:rohanbatrain/knowledge-base.git
+git clone git@github.com:rohanbatrain/scripts.git
