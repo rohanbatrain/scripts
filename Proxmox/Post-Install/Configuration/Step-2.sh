369c960c8b5bbed7489efcd097fd74910acdd985
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-08-27 17:17:34 +0530

    Update Step-2.sh

--- Proxmox/Post-Install/Configuration/Step-2.sh
+++ Proxmox/Post-Install/Configuration/Step-2.sh
@@ -27,3 +27,9 @@ git clone git@github.com:rohanbatrain/second-brain-template.git
 git clone git@github.com:rohanbatrain/dotfiles.git
 git clone git@github.com:rohanbatrain/knowledge-base.git
 git clone git@github.com:rohanbatrain/scripts.git
+
+# Over to the next step
+wget https://raw.githubusercontent.com/rohanbatrain/scripts/main/Proxmox/Post-Install/Configuration/Step-3.sh
+chmod +x Step-3.sh
+rm /home/rohan/Step-2.sh
+bash Step-3.sh
