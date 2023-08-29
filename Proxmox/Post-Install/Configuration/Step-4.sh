8181d047bc9af04cc6e8054bc019522409e4c01c
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-08-29 11:27:28 +0530

    Update Step-4.sh

--- Proxmox/Post-Install/Configuration/Step-4.sh
+++ Proxmox/Post-Install/Configuration/Step-4.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 # We are going to symlink manjaro's Home directories, to be clear not the whole home directory rather it's required sub directories, why not the other way around, well proxmox is primarily gonna have a working full fledged VM so this dwm setup will not gonna be my main anyway, so instead of storing data here, i would store data on manjaro, which seems a better trade off.
+sudo mkdir /mnt/manjaro/
+sudo mount /dev/nvme0n1p4 /mnt/manjaro/
+# Now please note the /home/rohan directory on proxmox should be empty.
+cd /home/rohan
+## Symlinking most used directories
+for i in Applications  Builds  Desktop  Documents  Downloads  Music  Pictures  Projects  Public  Scripts  Templates  Videos  ; do ln -sf /mnt/manjaro/home/rohan/$i . ; done
 
