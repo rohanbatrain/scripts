7c9cd189cb03d709e7520216ef22afe0858749ec
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-09-04 15:44:17 +0530

    Update Step-3.sh
    Migrated git clone links to Repo-Clone.sh

--- OS-Specific/Proxmox/Post-Installation/Configuration/Step-3.sh
+++ OS-Specific/Proxmox/Post-Installation/Configuration/Step-3.sh
@@ -1,42 +1,24 @@
 #!/bin/bash 
 
-cd home/rohan//Projects 
-git clone git@github.com:rohanbatrain/second-brain-api.git
-git clone git@github.com:rohanbatrain/educational-projects.git
-git clone git@github.com:rohanbatrain/productivity-suite.git
-git clone git@github.com:rohanbatrain/second-brain-template.git
-git clone git@github.com:rohanbatrain/dotfiles.git
-git clone git@github.com:rohanbatrain/knowledge-base.git
-git clone git@github.com:rohanbatrain/scripts.git
+cd home/rohan/Projects/ 
+wget https://raw.githubusercontent.com/rohanbatrain/scripts/main/Application-Specific/Github/Repo-Clone.sh
+bash Repo-Clone.sh
 
-
-# Going to home of rohan
-cd /home/rohan/
-
-# Making a build directory
-mkdir Builds
-cd Builds
-
-# Fetching my github repos specific to developement
-git clone git@github.com:rohanbatrain/suckless-dmenu.git
-git clone git@github.com:rohanbatrain/suckless-st.git
-git clone git@github.com:rohanbatrain/suckless-dwm.git
-
-# Building them 
+# Building from sources 
 ## DWM
 cd suckless-dwm/V6.4/
 sudo make clean install
-cd ../../
+cd ../
 
 ## DMENU
 cd suckless-dmenu/V5.2/
 sudo make clean install
-cd ../../
+cd ../
 
 ## ST
 cd suckless-st/V0.9/
 sudo make clean install
-cd ../../
+cd ../
 
 
 # Getting out of build directory
