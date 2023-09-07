9d81934b879f0b4f803b6d4bcbbd8a268a0d0372
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-09-07 15:45:09 +0530

    Update arch-install.sh
    Added script fetching logic on arch install

--- OS-Specific/Arch-Linux/Installation/arch-install.sh
+++ OS-Specific/Arch-Linux/Installation/arch-install.sh
@@ -3,15 +3,19 @@ read -p "Are you installing for Single-Boot or Double-Boot? [1,2]: " choice_1
 if [[ $choice_1 == "1" ]]; then
 
   echo "Getting script for Single Boot"
+  
 
 elif [[ $choice_1 == "2" ]]; then
 
   echo "Getting script for Dual Boot"
+  wget https://raw.githubusercontent.com/rohanbatrain/scripts/main/OS-Specific/Arch-Linux/Installation/Dual-Boot/arch-step-1.sh
+  bash arch-step-1.sh
 
 else
 
   echo "Unknown input, ungraceful exit"
-
+  wget https://raw.githubusercontent.com/rohanbatrain/scripts/main/OS-Specific/Arch-Linux/Installation/Single-Boot/arch-step-1.sh
+  bash arch-step-1.sh
   exit
 
 fi
