852b958ed643a07d96ca26a052dd7743d9fe2c4a
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2023-11-22 01:11:06 +0530

    Create pfsense_2.7.1.sh

---
+++ Application-Specific/Web-Scraping/pfsense/pfsense_2.7.1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /var/lib/vz/template/iso
+wget https://sgpfiles.netgate.com/mirror/downloads/pfSense-CE-2.7.1-RELEASE-amd64.iso.gz # singapore mirror 
+gunzip pfSense-CE-2.7.1-RELEASE-amd64.iso.gz
