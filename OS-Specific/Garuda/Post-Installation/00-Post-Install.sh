9f090f3652f818699d777b0fb88cdb3c8d224a45
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-01-02 21:56:07 +0530

    Update 00-Post-Install.sh

--- OS-Specific/Garuda/Post-Installation/00-Post-Install.sh
+++ OS-Specific/Garuda/Post-Installation/00-Post-Install.sh
@@ -1,1 +1,5 @@
-
+# I faced garuda-update pgp issue, which is fixed using the following commands.
+sudo rm -rf /etc/pacman.d/gnupg
+sudo pacman-key -init
+sudo pacman-key-populate archlinux
+sudo pacman-key-populate chaotic
