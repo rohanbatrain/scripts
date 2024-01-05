6af975947a744c487a4c1515f15436ad05ddda56
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-01-05 18:14:34 +0530

    Update Step-4B.sh

--- OS-Specific/Proxmox/Post-Installation/Configuration/Step-4B.sh
+++ OS-Specific/Proxmox/Post-Installation/Configuration/Step-4B.sh
@@ -1,3 +1,6 @@
 # Garuda Dual boot
 cd /home/rohan
 mkdir Applications Builds Desktop Documents Downloads Music Pictures Public Templates Videos ;
+
+
+# In this installation we are NOT linking both of the operating systems. We shall link a single shared folder if necessary/required.
