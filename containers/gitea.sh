5f9da787a7b6f6e4c6cd7112b7d23b48c13b3e96
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-01-07 15:34:41 +0530

    Update gitea.sh

--- containers/gitea.sh
+++ containers/gitea.sh
@@ -7,3 +7,21 @@ apt updat
 wget -O gitea https://dl.gitea.com/gitea/1.21.3/gitea-1.21.3-linux-amd64
 chmod +x gitea
 
+# Creating new user
+adduser \
+   --system \
+   --shell /bin/bash \
+   --gecos 'Git Version Control' \
+   --group \
+   --disabled-password \
+   --home /home/git \
+   git
+
+# Creating required directory structure
+mkdir -p /var/lib/gitea/{custom,data,log}
+chown -R git:git /var/lib/gitea/
+chmod -R 750 /var/lib/gitea/
+mkdir /etc/gitea
+chown root:git /etc/gitea
+chmod 770 /etc/gitea
+
