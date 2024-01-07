ad036b63c1df0097496c676eafac3e8412a50700
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-01-07 14:56:14 +0530

    Update gitea.sh

--- containers/gitea.sh
+++ containers/gitea.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-apt update
+# Updating the system
+apt updat
+
+# Installing gitea
 wget -O gitea https://dl.gitea.com/gitea/1.21.3/gitea-1.21.3-linux-amd64
 chmod +x gitea
-
-## Checking speed in vm, at the moment i am finding issues with my internet speed in lxc containers, maybe my mirrors are buggy?  
-### Turns out enabling firewall on proxmox was not a good idea.
-sudo apt install speedtest-cli
-speedtest-cli --secure
 
