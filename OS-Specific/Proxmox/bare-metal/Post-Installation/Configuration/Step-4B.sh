8b0dd2bf3b96664bfd7acf1966a4bd700eb4b2b0
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-01-12 21:20:24 +0530

    Update Step-4B.sh

--- OS-Specific/Proxmox/bare-metal/Post-Installation/Configuration/Step-4B.sh
+++ OS-Specific/Proxmox/bare-metal/Post-Installation/Configuration/Step-4B.sh
@@ -2,5 +2,6 @@
 cd /home/rohan
 mkdir Applications Builds Desktop Documents Downloads Music Pictures Public Templates Videos ;
 
-
 # In this installation we are NOT linking both of the operating systems. We shall link a single shared folder if necessary/required.
+cd Applications
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.3/Obsidian-1.5.3.AppImage
