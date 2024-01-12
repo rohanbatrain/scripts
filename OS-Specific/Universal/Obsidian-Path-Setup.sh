56232ef0a4f14e9d2181aad6f06056e81025e8a2
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-01-12 22:37:26 +0530

    Update Obsidian-Path-Setup.sh

--- OS-Specific/Universal/Obsidian-Path-Setup.sh
+++ OS-Specific/Universal/Obsidian-Path-Setup.sh
@@ -1,4 +1,4 @@
 cd /usr/local/bin
 wget https://raw.githubusercontent.com/rohanbatrain/scripts/main/OS-Specific/Universal/obsidian
 chmod +x obsidian
-chmod +x ./home/rohan/Applications/Obsidian*AppImage
+chmod +x /home/rohan/Applications/Obsidian*AppImage
