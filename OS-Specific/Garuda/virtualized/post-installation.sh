79a78004e14387fdb860767371baa7be559ee215
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-01-14 20:03:09 +0530

    Update post-installation.sh

--- OS-Specific/Garuda/virtualized/post-installation.sh
+++ OS-Specific/Garuda/virtualized/post-installation.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo pacman -S obsidian obs-studio vscodium 
+sudo pacman -S obsidian obs-studio vscodium
+
+cd /tmp
+wget https://us.download.nvidia.com/XFree86/Linux-x86_64/535.146.02/NVIDIA-Linux-x86_64-535.146.02.run
+
+sudo bash NVIDIA*.run
