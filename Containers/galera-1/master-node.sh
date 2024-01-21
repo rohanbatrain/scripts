cf957aa6827b15e9af240a977e28379d08405391
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-01-21 23:41:33 +0530

    Update master-node.sh

--- Containers/galera-1/master-node.sh
+++ Containers/galera-1/master-node.sh
@@ -3,3 +3,6 @@ apt update && apt upgrade
 apt install mariadb-server
 mysql_secure_installation
 sudo nano /etc/mysql/mariadb.conf.d/60-galera.cnf
+sudo systemctl stop mariadb
+sudo galera_new_cluster
+mysql -u root -p
