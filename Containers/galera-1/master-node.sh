d40b0f66f43245f19c036f97db08d0035ae85712
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-01-25 21:50:39 +0530

    Delete Containers/galera-1/master-node.sh

--- Containers/galera-1/master-node.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt update && apt upgrade
-apt install mariadb-server
-mysql_secure_installation
-sudo nano /etc/mysql/mariadb.conf.d/60-galera.cnf
-sudo systemctl stop mariadb
-sudo galera_new_cluster
-mysql -u root -p
