1d9cecfc2e2f455f2b56246b0793afbe33d57164
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-01-31 10:14:07 +0530

    Update 00-Post-Install.sh

--- OS-Specific/Manjaro/bare-metal/Post-Installation/00-Post-Install.sh
+++ OS-Specific/Manjaro/bare-metal/Post-Installation/00-Post-Install.sh
@@ -4,14 +4,8 @@
 
 ### Prerequisitie :- Please copy your ssh keys to .ssh directory.
 
-cd ~/.ssh/
-chmod 400 id_rsa
-git config --global user.name "Rohan Batra"
-git config --global user.email "116573125+rohanbatrain@users.noreply.github.com"
-git config --global gpg.format ssh
-git config --global commit.gpgsign true
-git config --global user.signingkey ~/.ssh/id_rsa.pub
-ssh -T git@github.com
+wget https://raw.githubusercontent.com/rohanbatrain/scripts/main/Application-Specific/Git/initial-setup.sh
+bash initial-setup.sh
 
 ## Repo sync
 sudo pacman-mirrors --country India,Singapore  && sudo pacman -Syu
