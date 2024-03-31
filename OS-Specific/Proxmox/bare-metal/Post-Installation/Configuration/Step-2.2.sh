cb4989b15e87ae9a550da11e5c0c31ce61386c3f
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-03-31 17:59:32 +0530

    Update Step-2.2.sh

--- OS-Specific/Proxmox/bare-metal/Post-Installation/Configuration/Step-2.2.sh
+++ OS-Specific/Proxmox/bare-metal/Post-Installation/Configuration/Step-2.2.sh
@@ -1,6 +1,37 @@
-# This script should be ran after "Login with your created user (USERNAME) and connect to the the WIFI." Step
+# Should be ran by non root user
 
 
-# Restart Network Manager
-sudo service NetworkManager restart
 
+# Cloning repositories from GitHub
+cd /home/rohan/Projects/Public/ || exit 1
+wget https://raw.githubusercontent.com/rohanbatrain/scripts/main/Application-Specific/Github/Repo-Clone.sh
+if [ $? -ne 0 ]; then
+    echo "Failed to download Repo-Clone.sh script."
+    exit 1
+fi
+
+bash Repo-Clone.sh
+if [ $? -ne 0 ]; then
+    echo "Failed to execute Repo-Clone.sh script."
+    exit 1
+fi
+
+# Appimages
+## Creating Applications directory
+cd ~ || exit 1
+mkdir -p Applications
+cd Applications || exit 1
+
+### Obsidian
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.3.7/Obsidian-1.3.7.AppImage
+if [ $? -ne 0 ]; then
+    echo "Failed to download Obsidian AppImage."
+    exit 1
+fi
+
+## Making all AppImages executable
+chmod +x *.AppImage
+if [ $? -ne 0 ]; then
+    echo "Failed to make AppImages executable."
+    exit 1
+fi
