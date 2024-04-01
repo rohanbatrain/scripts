1050ba2b727db175b12c1ee919c2b76e4515d24a
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-04-01 08:19:23 +0530

    Made some changes; I am not sure if they are correct and going to work just fine in the long run, but if it works now, it should work in the future and leaving this commit msg so that i can know that this is the potential commit which can break stuff

--- OS-Specific/Proxmox/bare-metal/Post-Installation/Configuration/Step-2.3.sh
+++ OS-Specific/Proxmox/bare-metal/Post-Installation/Configuration/Step-2.3.sh
@@ -1,4 +1,9 @@
-# This script should be ran after "Login with your created user (USERNAME) and connect to the the WIFI." Step
+# This script should be ran after "Login with your created user (USERNAME) and connect to the the WIFI." Step which is as followed,
+## DNS SERVERS : 1.1.1.1
+## address : 10.0.0.2
+## netmask : 255.255.255.0
+## gateway : 10.0.0.1
+
 
 
 # Restart Network Manager
@@ -7,15 +12,33 @@ sudo service NetworkManager restart
 wifi=$(cat ~/wifi_dev)
 
 tee << EOF >> /etc/network/interfaces
+auto lo
+iface lo inet loopback
+
+iface enp3s0 inet manual
+
 auto vmbr0
 iface vmbr0 inet static
-        address 10.0.0.1/24
+        address 10.0.0.2/24
+        gateway 10.0.0.1
+        bridge-ports enp3s0
+        bridge-stp off
+        bridge-fd 0
+
+
+
+auto vmbr2
+iface vmbr2 inet static
+        address 12.0.0.1/24
         bridge-ports none
         bridge-stp off
         bridge-fd 0
         post-up echo 1 > /proc/sys/net/ipv4/ip_forward
-        post-up iptables -t nat -A POSTROUTING -s '10.0.0.1/24' -o $wifi -j MASQUERADE
-        post-down iptables -t nat -D POSTROUTING -s '10.0.0.1/24' -o $wifi -j MASQUERADE
+        post-up iptables -t nat -A POSTROUTING -s '12.0.0.1/24' -o $wifi -j MASQUERADE
+        post-down iptables -t nat -D POSTROUTING -s '12.0.0.1/24' -o $wifi -j MASQUERADE
+
+
+
 EOF
 exit
 
@@ -24,9 +47,9 @@ exit
 
 tee << EOF > /etc/dnsmasq.conf
 domain=sitar-pve.rohanbatra.in
-interface=vmbr0
-dhcp-range=10.0.0.50,10.0.0.250,24h
-dhcp-option=vmbr0,3,10.0.0.1
+interface=vmbr2
+dhcp-range=12.0.0.2,12.0.0.254,24h
+dhcp-option=vmbr2,3,12.0.0.1
 server=1.1.1.1
 server=8.8.8.8
 dhcp-leasefile=/var/lib/misc/dnsmasq.leases
