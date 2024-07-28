e9c1fae7ac938f6c6be1003e5157bcae73829bed
Rohan Batra <116573125+rohanbatrain@users.noreply.github.com>
2024-07-28 17:55:33 +0530

    Update Step-1.2.sh

--- OS-Specific/Proxmox/bare-metal/Post-Installation/Configuration/Step-1.2.sh
+++ OS-Specific/Proxmox/bare-metal/Post-Installation/Configuration/Step-1.2.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-# Setting up ssh 
+# Ask the user if they want to set up GitHub and SSH keys now
+read -p "Do you need to set up GitHub and SSH keys now? Y/N: " SETUP_GITHUB_SSH
+SETUP_GITHUB_SSH=${SETUP_GITHUB_SSH^^} # Convert to uppercase
 
-mkdir -p ~/.ssh
-read -p "Have you copied the id_rsa and id_rsa.pub keys to your '~/.ssh' Directory? Y/N: " SSH_CONFIRMATION
-SSH_CONFIRMATION=${SSH_CONFIRMATION^^} # Convert to uppercase
+if [ "$SETUP_GITHUB_SSH" == "Y" ]; then
+    # Setting up SSH
+    mkdir -p ~/.ssh
+    read -p "Have you copied the id_rsa and id_rsa.pub keys to your '~/.ssh' Directory? Y/N: " SSH_CONFIRMATION
+    SSH_CONFIRMATION=${SSH_CONFIRMATION^^} # Convert to uppercase
 
-if [ "$SSH_CONFIRMATION" == "Y" ]; then
-    echo "Thank you for the confirmation, ssh keys already configured!"
-else
-    echo "Please copy the SSH keys to your '~/.ssh' directory before proceeding."
-    exit 1
+    if [ "$SSH_CONFIRMATION" == "Y" ]; then
+        echo "Thank you for the confirmation, SSH keys already configured!"
+    else
+        echo "Please copy the SSH keys to your '~/.ssh' directory before proceeding."
+        exit 1
+    fi
+
+    cd ~/.ssh/ || exit 1
+    sudo chmod 400 id_rsa
+    if [ $? -ne 0 ]; then
+        echo "Failed to set permissions for id_rsa."
+        exit 1
+    fi
+
+    git config --global user.name "Rohan Batra"
+    git config --global user.email "116573125+rohanbatrain@users.noreply.github.com"
+    git config --global gpg.format ssh
+    git config --global commit.gpgsign true
+    git config --global user.signingkey ~/.ssh/id_rsa.pub
+    ssh -T git@github.com
 fi
-
-cd ~/.ssh/ || exit 1
-sudo chmod 400 id_rsa
-if [ $? -ne 0 ]; then
-    echo "Failed to set permissions for id_rsa."
-    exit 1
-fi
-
-git config --global user.name "Rohan Batra"
-git config --global user.email "116573125+rohanbatrain@users.noreply.github.com"
-git config --global gpg.format ssh
-git config --global commit.gpgsign true
-git config --global user.signingkey ~/.ssh/id_rsa.pub
-ssh -T git@github.com
-
 
 # Flow control
 read -p "Are you installing on a Single boot environment? Y/N: " SINGLE_BOOT_CONFIRMATION
@@ -68,7 +72,7 @@ else
     elif [ "$SECONDARY_OS" == "B" ]; then
         # We are using Garuda as our secondary choice of operating system.
         # We are going to use Garuda config as it is from the Scripts folder.
-        curl -o Step-1.4B.sh https://raw.githubusercontent.com/rohanbatrain/scripts/main/OS-specific/Proxmox/bare-metal/Post-Installation/Configuration/Step-1.4B.sh
+        curl -o Step-1.4B.sh https://raw.githubusercontent.com/rohanbatrain/scripts/main/OS-specific/Proxmox/bare-metal/Post-installation/Configuration/Step-1.4B.sh
         chmod +x Step-1.4B.sh
         bash Step-1.4B.sh
     else
